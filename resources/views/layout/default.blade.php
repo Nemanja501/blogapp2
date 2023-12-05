f6a9a4e65e4b92acd50e5e242ccf8faf75ed6b1f
Nemanja501 <nemanjavulin5@gmail.com>
2023-12-05 20:23:06 +0100

    init

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('components.head')
+<body>
+    <div class="container">
+    @include('components.navigation')
+
+    @yield('content')
+
+    @include('components.footer')
+    </div>
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.default')
+
+
+
+@section('content')
+
+<h1>Welcome to my blog</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempora inventore repellendus a odio, sunt cum quas? Assumenda iste tempore nihil molestias odio, nulla totam, repellendus adipisci officiis ullam ad a officia id exercitationem? Iure dolorum tempore optio vero nam! Eius facilis aperiam delectus reprehenderit recusandae consequatur! Provident voluptatem tempora at distinctio minus illo architecto esse nemo aut aperiam dolorum libero rerum, maiores velit, iusto adipisci soluta, eum sit! Temporibus, eius qui repellat dolorum quibusdam debitis cupiditate velit! Cupiditate totam consectetur, mollitia provident numquam fuga repudiandae, sequi autem iusto modi omnis, rem est odit dolorem minus magnam maxime repellat consequuntur!</p>
+
+@endsection
